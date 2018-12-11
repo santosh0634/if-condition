19a88e481c07e856c8984a78907cb34652bd96b0
santosh0634 <45452833+santosh0634@users.noreply.github.com>
2018-12-11 21:14:11 +0530

    project 2

---
+++ car1.py
@@ -0,0 +1,17 @@
+#if else(example program)
+no1=int(input("enter the first number ="))
+no2=int(input("enter the second number = "))
+if no1>no2:   #if block statements
+    print(no1,"is greater")
+    print("new porsche cayenne car is awesome")
+    print("best in performance")
+    print("Porsche Cayenne is one of the globally renowned models from the luxury carmaker available in petrol and diesel fuel options")
+else:         #else block statements
+    print(no2,"is greater")
+    print("we have a new McLaren 720s")
+    print("The McLaren 720S is a British sports car designed and manufactured by McLaren Automotive")
+print("thanks")#other block statement
+
+
+
+
