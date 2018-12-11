9d3da4ada62e3487d57f2c7255f0580d72a1e86f
santosh0634 <45452833+santosh0634@users.noreply.github.com>
2018-12-11 20:42:29 +0530

    first project

---
+++ cars.py
@@ -0,0 +1,9 @@
+no1=int(input("enter first number:"))
+no2=int(input("enter second number"))
+if no1>no2:
+    print("no1:is greater")
+if no2>no1:
+    print("no2:is greater")
+if no1==no2:
+    print("both are same")
+print("welcome")
